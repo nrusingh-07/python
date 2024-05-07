a02c82abb29818cd1cab1c7c61e5422e478905ba
nrusi <nrusinghprasadswain@gmail.com>
2024-05-07 12:31:30 +0530

    Init

---
+++ operate.py
@@ -0,0 +1,8 @@
+a= 4
+b= a*10
+print(a,b)
+def variable():
+    a = input("5\n")
+
+
+    print(a + "b")
---
+++ test.py
@@ -0,0 +1,9 @@
+print('hello World')
+
+def name():
+    a = input("What is Your name?\n")
+
+    print(a + " is your Name")
+
+
+name()
